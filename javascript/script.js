6463d53484f4ab3763d6ed97fce70a10beeeb753
raulnasoliveira <raulnasoliveira@gmail.com>
2025-05-21 19:14:33 +0100

    Dropdown Changes

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,10 @@
 function toggleDropdown() {
     const menu = document.getElementById('dropdown-menu');
     if (menu) {
-      menu.classList.toggle('hidden');
+      menu.classList.toggle('show');
     }
-  }
-  
+}
+
 document.addEventListener('DOMContentLoaded', function () {
   const avatar = document.getElementById('profile-avatar');
   const dropdown = document.getElementById('dropdown-menu');
@@ -14,13 +14,13 @@ document.addEventListener('DOMContentLoaded', function () {
   // Show/hide on click
   avatar.addEventListener('click', function (event) {
     event.stopPropagation();
-    dropdown.classList.toggle('hidden');
+    dropdown.classList.toggle('show');
   });
 
   // Hide when clicking outside
   document.addEventListener('click', function (event) {
-    if (!dropdown.classList.contains('hidden') && !dropdown.contains(event.target)) {
-      dropdown.classList.add('hidden');
+    if (dropdown.classList.contains('show') && !dropdown.contains(event.target) && !avatar.contains(event.target)) {
+      dropdown.classList.remove('show');
     }
   });
 });
--- templates/common.tpl.php
+++ templates/common.tpl.php
@@ -87,11 +87,18 @@ function getRandomColorClass() {
     <?= $initial ?>
   </div>
   <div id="dropdown-menu" class="dropdown-menu hidden">
-    <a href="../pages/profile.php"><i class="fa fa-user"></i> Profile</a>
-    <a href="../pages/my_orders.php"><i class="fa fa-list"></i> My Orders</a>
-    <a href="../pages/my_services.php"><i class="fas fa-briefcase"></i> My Services</a>
-    <a href="../pages/messages.php"><i class="fa fa-envelope"></i> Messages</a>
-    <a href="../pages/settings.php"><i class="fa fa-cog"></i> Settings</a>
+    <form action="../pages/profile.php" method="get">
+      <button type="submit"><i class="fa fa-user"></i> Profile</button>
+    </form>
+    <form action="../pages/my_orders.php" method="get">
+      <button type="submit"><i class="fa fa-list"></i> My Orders</button>
+    </form>
+    <form action="../pages/my_services.php" method="get">
+      <button type="submit"><i class="fas fa-briefcase"></i> My Services</button>
+    </form>
+    <form action="../pages/messages.php" method="get">
+      <button type="submit"><i class="fa fa-envelope"></i> Messages</button>
+    </form>
     <form action="../actions/action_logout.php" method="post" style="margin:0;">
       <button type="submit" style="width:100%;text-align:left;"><i class="fa fa-door-open"></i> Logout</button>
     </form>
