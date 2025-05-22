b467f4fa00df8ad8e9cec5adc40fe1eed2b0a36f
Bernardo Neves <bernardodesousaneves@gmail.com>
2025-05-22 15:46:59 +0100

    base css added

--- templates/common.tpl.php
+++ templates/common.tpl.php
@@ -16,7 +16,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
     <!-- Global styles -->
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/base.css">
     
     <!-- Optional extra styles -->
     <?php if (!empty($extraCss)): ?>
