c2c8f5eb4b715adc83dd126cad81900ea413008b
Bernardo Neves <bernardodesousaneves@gmail.com>
2025-05-22 16:04:06 +0100

    forms css added

--- templates/common.tpl.php
+++ templates/common.tpl.php
@@ -18,6 +18,7 @@
     <!-- Global styles -->
     <link rel="stylesheet" href="../css/base.css">
     <link rel="stylesheet" href="../css/dropdown.css">
+    <link rel="stylesheet" href="../css/forms.css">
     
     <!-- Optional extra styles -->
     <?php if (!empty($extraCss)): ?>
